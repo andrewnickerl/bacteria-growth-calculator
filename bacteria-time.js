17dc1ff084082e422d3efe9b3fbfb2af6dd5512b
Andrew Nickerl <andrew.nickerl@gmail.com>
2019-12-24 05:41:53 -0500

    Create bacteria-time.js

---
+++ bacteria-time.js
@@ -0,0 +1,19 @@
+// YOUR CODE BELOW
+const bacteriaTime = (currentNum, targetNum) =>
+{
+    if (currentNum > targetNum) 
+    {
+        return 'targetNum must be larger than currentNum';
+    }
+    else
+    {
+        let doubleMin = 20;
+        let growth = currentNum * 2;
+        while (growth < targetNum)
+        {
+            growth *= 2;
+            doubleMin += 20;
+        }
+        return doubleMin;
+    }
+}
